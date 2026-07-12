bc6b0aae8c4e422e283a25016039783c43ba4381
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-07-12 17:30:32 +0530

    Clerk authentication setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
@@ -12,13 +14,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
+        <ClerkProvider>
         <html lang="en">
             <body className={`${outfit.className} antialiased`}>
+
                 <StoreProvider>
                     <Toaster />
                     {children}
                 </StoreProvider>
             </body>
         </html>
+        </ClerkProvider>
     );
 }
