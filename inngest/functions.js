8bdbf9d05f52c06f283e3660ec47ebc925741522
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-07-14 18:45:27 +0530

    Created first inngest function to syncUserCreation

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,3 +1,20 @@
 // src/inngest/functions.ts
+import { prisma } from "@/lib/prisma";
 import { inngest } from "./client";
 
+// Inngest Functions to save user data to a database
+export const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-create" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  },
+);
