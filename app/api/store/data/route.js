4fc53ce03809cce8d4518f79a871e60903038592
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-08-18 14:38:03 +0530

    Created API to return store data product for that store

---
+++ app/api/store/data/route.js
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+
+// Get store info & store products
+export async function GET(request) {
+  try {
+    // Get store username from query params
+    const { searchparams } = new URL(request.url);
+    const username = searchparams.get("username").toLowerCase();
+
+    if (!username) {
+      return NextResponse.json({ error: "missing username" }, { status: 400 });
+    }
+
+    // Get store info and inStock products with ratings
+    const store = await prisma.store.findUnique({
+      where: { username, isActive: true },
+      include: { Product: { include: { rating: true } } },
+    });
+
+    if (!store) {
+      return NextResponse.json({ error: "store not found" }, { status: 404 });
+    }
+    return NextResponse.json({ store });
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 },
+    );
+  }
+}
